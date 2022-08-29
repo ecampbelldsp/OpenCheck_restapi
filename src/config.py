49fb87023cea9b98560c6e672e1ff3704fb22945
Edward L. Campbell <77728779+ecampbelldsp@users.noreply.github.com>
2022-08-29 15:29:28 +0200

    tokens

--- src/config.py
+++ src/config.py
@@ -8,11 +8,11 @@ redirect_uri = "https://81ee-31-4-128-46.eu.ngrok.io"
 client_secret = 'gwSvTrFauEbt4OkKy1sVGCnB3hpcjf7I'
 
 scope = "write:guest read:guest write:reservation read:reservation"
-code_4_scope_guest_and_reservation = "dq5Fma8tKu0UheY0ZOe8Uv31fP9ybumSps_T72dGtoE"
+code_4_scope_guest_and_reservation = "A3lTM0r2Px9b7Z7RC62CdxhbYUZ3H80gy1YQTfXamYc"
 state_4_scope_guest_and_reservation = "fb08f0d2ff44e959fcd83dc20e58a8c0d729cf6f630c9f0883630"
 path_tokens = "data/tokens_guests_and_reservation.json"
 
-code_4_scope_payment_and_room = "UWXQi3Foixd3cNOJV6N7HOOoPW0FNeawSSjv4bg4Jws"
+code_4_scope_payment_and_room = "vu0MImXMfLmJLcBRBoZfUUXbCnmxsV6fL4-i_4jRWL8"
 state_4_scope_payment_and_room = "fb08f0d2ff44e959fcd83dc20e58a8c0d729cf6f630c9f340b048"
 path_tokens_payment_and_room = "data/tokens_payments_and_room.json"
 
